6fa59042309e05cdde36c442741891e67d8d9869
Your <vlasuyk.2015.1@mail.ru>
2022-07-18 14:01:45 +0300

    all

---
+++ ds 19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 19");
+Console.Write("Введите число пятизначное: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число пятизначное: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число пятизначное: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine("Введи правильное число");
